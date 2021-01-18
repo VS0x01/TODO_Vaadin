b592179f5b41c718af74c9aeb2e041698ef65289
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-01-18 18:11:21 +0200

    Add styles

--- src/main/java/com/example/MainView.java
+++ src/main/java/com/example/MainView.java
@@ -2,7 +2,9 @@ package com.example;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -11,12 +13,14 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route
+@CssImport("styles/shared-styles.css")
 public class MainView extends VerticalLayout {
 
     public MainView(MessageBean message) {
         VerticalLayout todosList = new VerticalLayout();
         TextField taskField = new TextField();
         Button addButton = new Button("Add");
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         addButton.addClickShortcut(Key.ENTER);
         addButton.addClickListener(click -> {
             Checkbox checkbox = new Checkbox(taskField.getValue());
